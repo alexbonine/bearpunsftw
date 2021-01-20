fd6b57a6e774efd4eab35da4155b64c32bfe6255
Alex Bonine <lemurdev@gmail.com>
2021-01-20 00:55:10 -0500

    First cut of Things to do

--- src/components/ThingsToDo/index.js
+++ src/components/ThingsToDo/index.js
@@ -1,8 +1,68 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import useComponentSize from "@rehooks/component-size";
+import {
+  Container,
+  FlexContainer,
+  Grid,
+  GridItem,
+  GridItemSpacer,
+  MapContainer,
+} from "./styles";
 
 const ThingsToDo = () => {
-  return <Container>Things To Do</Container>;
+  const stuffRef = useRef(null);
+  const [iframeHeight, setIframeHeight] = useState(640);
+  const size = useComponentSize(stuffRef);
+  console.log(size);
+
+  useEffect(() => {
+    setIframeHeight(Math.max(size.height, 640));
+    console.log("useEffect", size.height, Math.max(size.height, 640));
+  }, [size.width]);
+
+  return (
+    <Container>
+      <FlexContainer>
+        <MapContainer>
+          <iframe
+            src="https://www.google.com/maps/d/u/0/embed?mid=1cRUpLiSyc0-nIyghXZ_Pu6fcTwupOdsC"
+            width={480}
+            height={iframeHeight}
+            title="NYC Map of Things To Do"
+          ></iframe>
+        </MapContainer>
+        <Grid ref={stuffRef}>
+          <GridItem>
+            <h5>Chomp Chomp Chomp</h5>
+            <p>Restauarnt 1</p>
+            <p>Restauarnt 2</p>
+            <p>Restauarnt 3</p>
+            <p>Restauarnt 4</p>
+            <p>Restauarnt 5</p>
+          </GridItem>
+          <GridItemSpacer />
+          <GridItemSpacer />
+          <GridItem>
+            <h5>Think Walk See</h5>
+            <p>The Metropolitan Museum of Art</p>
+            <p>Central Park</p>
+            <p>The High Line</p>
+            <p>Thing 4</p>
+            <p>Thing 5</p>
+          </GridItem>
+          <GridItem>
+            <h5>Glug Glug Glug</h5>
+            <p>Bar 1</p>
+            <p>Bar 2</p>
+            <p>Bar 3</p>
+            <p>Bar 4</p>
+            <p>Bar 5</p>
+          </GridItem>
+          <GridItemSpacer />
+        </Grid>
+      </FlexContainer>
+    </Container>
+  );
 };
 
 export default ThingsToDo;
--- src/components/ThingsToDo/styles.js
+++ src/components/ThingsToDo/styles.js
@@ -1,10 +1,43 @@
 import styled from "@emotion/styled";
+import mq from "styles/mq";
 
 export const Container = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
+  background-color: #b5e3f5;
+`;
+
+export const FlexContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: violet;
+  flex-wrap: wrap;
+  padding: 100px;
+`;
+
+export const MapContainer = styled.div``;
+
+export const Grid = styled.div`
+  flex: 1;
+  padding-left: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-gap: 20px;
+  grid-column-gap: 20px;
+`;
+
+export const GridItem = styled.div`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  // align-items: center;
+  ${mq({
+    gridColumn: ["span 3", "span 3", "span 2", "span 2"],
+    // padding: ["0", "0", "60px 0", "60px 0"],
+  })}
+`;
+
+export const GridItemSpacer = styled.div`
+  ${mq({
+    gridColumn: ["none", "none", "span 1", "span 1"],
+  })}
 `;
