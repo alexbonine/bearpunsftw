94e9103b427d9b2e89c71c49cbe201c7a5321ef0
Alex Bonine <lemurdev@gmail.com>
2021-01-22 22:23:42 -0500

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,34 @@ module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        id: "258471519",
-        defaultDataLayer: { platform: "gatsby" },
+        // your google analytics tracking id
+        trackingId: `G-09JRJVJEB8`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "258471519",
+    //     defaultDataLayer: { platform: "gatsby" },
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "GA-TRACKING_ID", // Google Analytics / GA
+    //       "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
+    //   },
+    // }
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
