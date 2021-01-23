542c2cb5d9f2ccfdd42af67e29940249c90aab0e
Alex Bonine <lemurdev@gmail.com>
2021-01-22 22:32:45 -0500

    Clean up google analytics missteps

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
-        // your google analytics tracking id
         trackingId: `G-09JRJVJEB8`,
         // Puts tracking script in the head instead of the body
         head: false,
@@ -18,30 +17,6 @@ module.exports = {
         anonymize: true,
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-google-tagmanager",
-    //   options: {
-    //     id: "258471519",
-    //     defaultDataLayer: { platform: "gatsby" },
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
-    //     trackingIds: [
-    //       "GA-TRACKING_ID", // Google Analytics / GA
-    //       "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-    //       "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-    //     ],
-    //   },
-    // }
-    // {
-    //   resolve: "gatsby-plugin-google-analytics",
-    //   options: {
-    //     trackingId: "258471519",
-    //   },
-    // },
     {
       resolve: `gatsby-alias-imports`,
       options: {
